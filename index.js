d7cfc2532399f5c024bb6c2b0561ac3a7b08ce52
Devin Rooney <devroo2000@gmail.com>
2019-12-11 01:48:16 -0500

    bug fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from './node_modules/react/index.js';
+import ReactDOM from './node_modules/react-dom/index.js';
 import App from './app.js';
 
 ReactDOM.render(App, document.getElementById('app'));
